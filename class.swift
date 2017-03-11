bca2305d5d78d3514216d996ba4a2bdd54ea01dd
Marek Pridal <m.pridal29@gmail.com>
2017-03-11 22:26:17 +0100

    import added

--- class.swift
+++ class.swift
@@ -1,22 +1,22 @@
 //: Playground - noun: a place where people can play
-
+import Foundation
 class Animal
 {
     private var name:String
     private var age:Int
-    
+
     public init()
     {
         self.name = "noName"
         self.age = -1
     }
-    
+
     public init(jmeno:String,vek:Int)
     {
         self.name=jmeno
         self.age=vek
     }
-    
+
     public func getDetails() -> String
     {
         return "Zvire se jmenuje "+self.name+" a je stare \(age) let(roků)"
@@ -26,24 +26,24 @@ class Animal
 class Dog:Animal
 {
     private var vaha:Int
-    
+
     func woof()->String
     {
         return "Haf"
     }
-    
+
     public override init()
     {
         self.vaha = -1
         super.init()
     }
-    
+
     public init(jmeno:String,vek:Int,vaha:Int)
     {
         self.vaha = vaha
         super.init(jmeno: jmeno, vek: vek)
     }
-    
+
     public override func getDetails() -> String
     {
         return super.getDetails()+" a jeho vaha je \(vaha) kg"

052bc160a98f7c7fa8329ed23fef7c487f8ef78e
Marek Pridal <m.pridal29@gmail.com>
2017-04-23 19:06:57 +0200

    hello world in packageManager

---
+++ packageDemo/Sources/main.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+print("Hello World!")
+
+if let input = readLine()
+{
+    print("Your input \(input)")
+}
---
+++ packageDemo/Tests/LinuxMain.swift
@@ -0,0 +1,6 @@
+import XCTest
+@testable import packageDemoTests
+
+XCTMain([
+    testCase(packageDemoTests.allTests),
+])

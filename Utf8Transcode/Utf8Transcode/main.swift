46aa2bc3d4392a890c481545be3433155e5632b9
Marek Pridal <marek.pridal@myair.cz>
2017-11-16 20:22:13 +0100

    Demo project for transcoding text files with Windows-1250 coding to UTF8 coding

---
+++ Utf8Transcode/Utf8Transcode/main.swift
@@ -0,0 +1,89 @@
+//
+//  main.swift
+//  Utf8Transcode
+//
+//  Created by Marek Přidal on 16.11.17.
+//  Copyright © 2017 Marek Přidal. All rights reserved.
+//
+import Foundation
+
+//MARK: - Constants
+let fileManager = FileManager.default
+let originalDefaultEncoding = String.Encoding.windowsCP1250
+let transcodedDefaultEncoding = String.Encoding.utf8
+
+//MARK: - Functions
+func getContent(ofFile path:String, encoding: String.Encoding)throws -> String {
+    return try String(contentsOfFile: path, encoding: encoding)
+}
+
+func checkIfFileExistsAndHaveContent(file: String) -> Bool {
+    if let content = try? getContent(ofFile: file, encoding: transcodedDefaultEncoding) {
+        return !content.isEmpty
+    }
+    return false
+}
+
+func printContentOfDirectory(atPath path: String) {
+    print("Files in directory 📂")
+    if let content = (try? fileManager.contentsOfDirectory(atPath: path)) {
+        for file in content.filter({!$0.hasPrefix(".")}) {
+            print("📄 " + file)
+        }
+    } else {
+        print("Folder is empty 💨")
+    }
+}
+
+func transcodeFile(withName fileName:String)throws {
+    let original = try getContent(ofFile: fileName, encoding: originalDefaultEncoding)
+    print("What's your name for transcoded file? (empty for same name)")
+    var newName = readLine(strippingNewline: true)
+    if newName?.isEmpty ?? true {
+        newName = fileName
+    }
+    try original.write(toFile: newName!, atomically: true, encoding: transcodedDefaultEncoding)
+    if checkIfFileExistsAndHaveContent(file: newName!) {
+        print("Success! 🎉 🍾")
+        print("You can find new transcoded file at path \(newName!) ⬅️")
+    } else {
+        print("Error 😞")
+        print("Something went wrong, sorry 😔")
+    }
+}
+
+//MARK: - Main
+func main() {
+    print("➡️ Current path \(fileManager.currentDirectoryPath)\n")
+    printContentOfDirectory(atPath: fileManager.currentDirectoryPath)
+    
+    print("\nWrite name of file 📄")
+    guard var fileName = readLine(strippingNewline: true) else {
+        print("Cannot get name of your file, sorry ❌")
+        exit(1)
+    }
+    
+    if !fileName.hasPrefix("/") {
+        fileName = fileManager.currentDirectoryPath + "/" + fileName
+    }
+    
+    if !fileName.hasSuffix(".srt") && !fileName.hasSuffix(".txt") {
+        print("Please provide file prefix ")
+        guard var prefix = readLine(strippingNewline: true) else {
+            print("Cannot get your input ❌")
+            exit(1)
+        }
+        if !prefix.hasPrefix(".") {
+            prefix = "." + prefix
+        }
+        fileName.append(prefix)
+    }
+    
+    do{
+        try transcodeFile(withName: fileName)
+    }catch let e {
+        print(e.localizedDescription)
+    }
+}
+//MARK: - Run main function
+main()
